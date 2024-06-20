b513c5c382fb4484620c57fa4e196037aff58e3b
XJ-L <132251270+I-WantMoney@users.noreply.github.com>
2024-06-20 11:17:59 +0900

    Update vectorDB_create.py

--- vectorDB_create.py
+++ vectorDB_create.py
@@ -1,7 +1,7 @@
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores.chroma import Chroma
 
-# ベクトルストア生成
+# ベクトルストア作成
 def get_vectorstore(chunks):
     embeddings = OpenAIEmbeddings()
     vectorstore = Chroma.from_documents(documents=chunks,embedding=embeddings)
