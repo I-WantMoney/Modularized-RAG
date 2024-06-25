f868b45ca61b1be32dcaf70493207e39b74b35d4
XJ-L <132251270+I-WantMoney@users.noreply.github.com>
2024-06-25 13:39:15 +0900

    Add files via upload

--- response_gen.py
+++ response_gen.py
@@ -21,9 +21,19 @@ load_dotenv()
 llm_model = os.environ["OPENAI_API_MODEL"]
 
 def get_context_retriever_chain(vector_store):
-    llm = ChatOpenAI(model=llm_model) # カッコ内でapi-keyの指定、モデルの指定などができます。コードの先頭にdotenvを使ったので、自動的に.envファイルからapi-keyを取得します
+    llm = ChatOpenAI(model=llm_model,max_tokens = 4096) # カッコ内でapi-keyの指定、モデルの指定などができます。コードの先頭にdotenvを使ったので、自動的に.envファイルからapi-keyを取得します
     retriever = vector_store.as_retriever(search_kwargs={"k": 1})
+    # new---------------------------------
+    contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+    )
+    # ------------------------------------
     prompt = ChatPromptTemplate.from_messages([
+        ("system", contextualize_q_system_prompt),
         MessagesPlaceholder(variable_name="chat_history"),
         ("user","{input}"),
         ("user","Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
@@ -35,9 +45,16 @@ def get_context_retriever_chain(vector_store):
 
 def get_conversational_rag_chain(retriever_chain):
     
-    llm = ChatOpenAI(model=llm_model) # カッコ内でapi-keyの指定、モデルの指定などができます。コードの先頭にdotenvを使ったので、自動的に.envファイルからapi-keyを取得します
+    llm = ChatOpenAI(model=llm_model,max_tokens = 4096) # カッコ内でapi-keyの指定、モデルの指定などができます。コードの先頭にdotenvを使ったので、自動的に.envファイルからapi-keyを取得します
+    # new-----------------------------------------
+    system_prompt = (
+        "You are an assistant for question-answering tasks."
+        "If you don't know the answer, just that say you don't know."
+        "Your answer should be based on the below context:\n\n{context}"
+    )
+    # --------------------------------------------
     prompt = ChatPromptTemplate.from_messages([
-        ("system","Answer the user's questions based on the below context:\n\n{context}"),
+        ("system",system_prompt),
         MessagesPlaceholder(variable_name="chat_history"),
         ("user","{input}")
     ])
@@ -49,7 +66,7 @@ def get_conversational_rag_chain(retriever_chain):
 def get_response(user_input):
     
     
-    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)    
+    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
     conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
 
     response = conversation_rag_chain.invoke({
